ea44a0bddf4b981ad40ee4a3965e477529d25834
jameseclement <jameseclement@gmail.com>
2018-12-12 19:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ function getCart() {
 }
 
 function setCart(c) {
-  cart = c;
+  cart = c
   return cart;
 }
 
 function addToCart(item) {
  // write your code here
+  var price = Math.floor(Math.random() * 100) +1
+ cart.push({ itemName : item , itemPrice: price});
+ 
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  } else if (cart.length ===1) {
+   var contents = "In your cart, you have " + `${cart[0].itemName} at $${cart[0].itemPrice}.`
+    } else if (cart.length === 2){
+     contents = "In your cart, you have "
+      contents = contents + `${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+    }
+      else {
+         contents = "In your cart, you have "
+         contents = contents + `${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
+      }
+ 
+   return contents
 }
+
 
 function total() {
   // write your code here
